4f2d9b9aa3c020e501338519af68269e64b3a695
Soumava Paul <soumava@mdbrd.in>
2020-09-01 23:22:20 +0530

    added comments

--- src/server.ts
+++ src/server.ts
@@ -8,11 +8,13 @@ import * as Hapi from "@hapi/hapi";
 const init = async () => {
   const { config } = require("./config");
   let cron = require("node-cron");
-  // cONFIGURE SERVER CREDS FROM CONFIG FILE
+
+  // CONFIGURE SERVER CREDS FROM CONFIG FILE
   const server = Hapi.server({
     port: config.api.port,
     host: config.api.host,
   });
+
   // CHECKS THE CURRENT TIME IN IST
   function getTimeForMail() {
     let date_ob = new Date();
@@ -34,6 +36,7 @@ const init = async () => {
       subject: "Hello",
       text: "Hey There ! Hope you doing well!",
     };
+
     // SEND ONLY IF ITS THE 2ND OF SEPTEMBER, 10:00 PM
     if (hours == 22 && month == "09" && date == 2) {
       //FIRE MAIL
@@ -53,6 +56,7 @@ const init = async () => {
     getTimeForMail();
   });
 
+  //START SERVER
   await server.start();
   console.log(process.env.API_KEY);
 };
